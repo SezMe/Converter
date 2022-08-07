c316f8c48da610d51544593be04607fabca360a0
Roger <CAS.Roger@cox.net>
2022-08-07 10:22:56 -0700

    Initial commit

---
+++ Converters/DateToNull.vb
@@ -0,0 +1,20 @@
+﻿Imports System.Globalization
+
+Public Class DateToNull
+
+	Implements IValueConverter
+
+	Public Function Convert(value As Object, targetType As Type, parameter As Object, culture As CultureInfo) As Object Implements IValueConverter.Convert
+		Dim val As Date? = CDate(value)
+		Return If(val > #1/1/1900#, val, Nothing)
+	End Function
+	Public Function ConvertBack(value As Object, targetType As Type, parameter As Object, culture As CultureInfo) As Object Implements IValueConverter.ConvertBack
+		If IsDate(value) Then
+			Dim val As Date = CDate(value)
+			Return If(val > #1/1/1900#, val, Nothing)
+		Else
+			Return Nothing
+		End If
+	End Function
+
+End Class
